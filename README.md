f927a6fba8832af700620bd6a32d300f9d3e2005
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-02-15 11:24:20 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# django-app-webpack-starterkit
---
+++ my_app/src/scripts/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+if (module.hot) {
+  module.hot.accept()
+}
+
+import 'babel-polyfill'
+
+// style
+import '../styles/index.scss'
+
+
+console.log('hello!')
